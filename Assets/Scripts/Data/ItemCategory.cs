7edda282b70c4b08cebb864a166c8058542cb20a
Nadya Djojosantoso <hello@nadyafebi.com>
2026-06-19 19:32:42 -0400

    Create item categories and effects

---
+++ Assets/Scripts/Data/ItemCategory.cs
@@ -0,0 +1,7 @@
+public enum ItemCategory
+{
+    Material,
+    Weapon,
+    Shield,
+    Potion,
+}
--- Assets/Scripts/Data/ItemData.cs
+++ Assets/Scripts/Data/ItemData.cs
@@ -11,12 +11,33 @@ public class ItemData : ScriptableObject
     [SerializeField]
     private Sprite _icon;
 
+    [Header("Category")]
+    [SerializeField]
+    private ItemCategory _category;
+
+    [Header("Effects")]
+    [Tooltip("Weapon only: bonus added to base attack.")]
+    [SerializeField]
+    private int _attackBonus;
+
+    [Tooltip("Shield only: bonus added to base defense.")]
+    [SerializeField]
+    private int _defenseBonus;
+
+    [Tooltip("Potion only: HP restored on use.")]
+    [SerializeField]
+    private int _healAmount;
+
     #endregion
 
     #region Public Properties
 
     public string DisplayName => _displayName;
     public Sprite Icon => _icon;
+    public ItemCategory Category => _category;
+    public int AttackBonus => _attackBonus;
+    public int DefenseBonus => _defenseBonus;
+    public int HealAmount => _healAmount;
 
     #endregion
 }
